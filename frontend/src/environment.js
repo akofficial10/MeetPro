e17d3d83c208047070ba19311e47cbb9f3d27a07
Ayush Kumar <akofficial1330@gmail.com>
2025-07-03 10:43:05 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = false;
 const server = IS_PROD
-  ? "https://apnacollegebackend.onrender.com"
+  ? "https://meetpro-rcw0.onrender.com"
   : "http://localhost:8000";
 
 export default server;
